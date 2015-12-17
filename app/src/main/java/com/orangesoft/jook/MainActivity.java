0bb4f4eb021ca9c6db96f60ee1de6f4f58a466a0
Will McKinley <willowmck@gmail.com>
2015-12-16 19:07:50 -0500

    Moved Settings to overflow menu

--- app/src/main/java/com/orangesoft/jook/MainActivity.java
+++ app/src/main/java/com/orangesoft/jook/MainActivity.java
@@ -1,36 +1,99 @@
 package com.orangesoft.jook;
 
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity
 {
+    private DrawerLayout drawerLayout;
+    private FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**
+         * Setup the DrawerLayout and NavigationView
+         */
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        /**
+         * Inflate the very first fragment
+         * Here, we are inflating the TabFragment as the first Fragment
+         */
+        fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.containerView, new TabFragment()).commit();
+
+        /**
+         * Setup click events on the Navigation View items
+         */
+        setupNavigation(navigationView);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setLogo(R.drawable.jook_icon);
-        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
-        setupViewPager(viewPager);
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(viewPager);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.app_name, R.string.app_name);
+        drawerLayout.setDrawerListener(drawerToggle);
+        drawerToggle.syncState();
     }
 
-    private void setupViewPager(ViewPager viewPager)
+    private void setupNavigation(NavigationView navigationView)
     {
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFrag(new SubsonicFragment(), "Settings");
-        adapter.addFrag(new PlaylistFragment(), "Playlists");
-        adapter.addFrag(new AlbumsFragment(), "Albums");
-        viewPager.setAdapter(adapter);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem)
+            {
+                drawerLayout.closeDrawers();
+
+                if (menuItem.getItemId() == R.id.nav_main)
+                {
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.containerView, new TabFragment()).commit();
+                }
+
+                if (menuItem.getItemId() == R.id.nav_settings)
+                {
+                    FragmentTransaction xfragmentTransaction = fragmentManager.beginTransaction();
+                    xfragmentTransaction.replace(R.id.containerView, new SubsonicFragment()).commit();
+                }
+
+                return false;
+            }
+        });
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // The action bar home/up action should open or close the drawer.
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Make sure this is the method with just 'Bundle' as the signature
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState)
+    {
+        super.onPostCreate(savedInstanceState);
+    }
+
 
 }
